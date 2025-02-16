7b0c39da6d00d97515eae539bd1e607c2b09e136
austinechisutia <austineakhonya624@gmail.com>
2025-02-16 22:43:37 +0300

    was about to be lazy

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -3,20 +3,14 @@ const setupScrolling = ()=>{
     const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
     const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-    container.forEach((item, i)=>{
-        let containerDimensions = item.getBoundingClientRect();
-        let containerWidth = containerDimensions.width;
+   container.forEach((item, i)=>{
+    let containerDimensions = item.getBoundingClientRect();
+    let  containerWidth = containerDimensions.width;
 
-        nxtBtn[i].addEventListener('click', ()=>{
-            item.scrollLeft += containerWidth;
-        })
-
-        preBtn[i].addEventListener('click', ()=>{
-            item.scrollLeft -= containerWidth;
-        })
-
+    nxtBtn[i].addEventListener('click', ()=>{
+        item.scrollLeft += containerWidth
     })
-
+   })
 
 
 }
